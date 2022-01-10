0fd536667a30fb07976656a1db6ccb6cb8817e4c
Seongjae <robbyl95@hanmail.net>
2022-01-10 11:17:14 +0900

    difficulty와 nonce 설정&채굴

--- main.js
+++ main.js
@@ -9,16 +9,28 @@ class Block {
     this.data = data;
     this.previousHash = this.previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
   // 위 요소들을 가져와서 블록마다 고유성을 부여 -> crypto-js 모듈 사용
   calculateHash() {
-    return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data)).toString();
+    return SHA256(this.index + this.timestamp + this.previousHash + JSON.stringify(this.data) + this.nonce).toString();
+  }
+  // Pow 채굴
+  mineBlock(difficulty) {
+    // 원하는 조건의 해시값을 찾을 때까지 while문 돌리기
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      // while문을 몇번 돌았는지 nonce로 확인할 수 있게 될 것
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("Block mined", this.hash);
   }
 };
 
 class BlockChain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 2;
   }
   createGenesisBlock() {
     return new Block(0, "01/02/2022", "Genesis Block", "0");
@@ -29,8 +41,10 @@ class BlockChain {
   addBlock(newBlock) {
     // 새로운 블록의 previousHash 값이 바로 전 블록의 해쉬값과 같은지 확인하고
     newBlock.previousHash = this.getLatestBlock().hash;
-    // 같다면 새로운 해쉬값을 계산해서 해쉬로 넣어주고
-    newBlock.hash = newBlock.calculateHash();
+    // // 같다면 새로운 해쉬값을 계산해서 해쉬로 넣어주고
+    // newBlock.hash = newBlock.calculateHash();
+
+    newBlock.mineBlock(this.difficulty);
     // chain에 새로운 블록을 넣어준다.
     this.chain.push(newBlock);
   }
@@ -53,14 +67,22 @@ class BlockChain {
   }
 }
 
-// 테스트
+
+
+
+// Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test
 let mimiCoin = new BlockChain();
+console.log("Data Mining block 1....");
 mimiCoin.addBlock(new Block(1, "01/02/2022", { amount: 4 }));
+console.log("Data Mining block 2....");
 mimiCoin.addBlock(new Block(2, "03/02/2022", { amount: 10 }));
 
-mimiCoin.chain[1].data = { amount: 77 }; // 데이터 변조 시도(1)
-mimiCoin.chain[1].hash = mimiCoin.chain[1].calculateHash(); // 데이터 변조 시도(2) .. 해쉬값도 계산해서 넣어주려 했으나~
-console.log("Is blockchain valid?", mimiCoin.isChainValid()); // 위에서 임의로 데이터를 바꾸려고 했기 때문에 false 나옴
+
+// // 2) 데이터 변조 시도
+// mimiCoin.chain[1].data = { amount: 77 }; // 데이터 변조 시도(1) .. 거래에서 실제보다 더 많이 받은 것처럼
+// mimiCoin.chain[1].hash = mimiCoin.chain[1].calculateHash(); // 데이터 변조 시도(2) .. 해쉬값도 계산해서 넣어주려 했으나~
+// console.log("Is blockchain valid?", mimiCoin.isChainValid()); // 위에서 임의로 데이터를 바꾸려고 했기 때문에 false 나옴
+// // 1) 코인들 잘 보이는지 확인
 // console.log(JSON.stringify(mimiCoin, null, 4));
 
 
